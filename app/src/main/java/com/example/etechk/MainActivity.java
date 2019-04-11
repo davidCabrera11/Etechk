e5954fdb8d4e78ff876b02f448827876e4a437b5
davidCabrera11 <davidcabrera052196@gmail.com>
2019-04-10 20:14:46 -0700

    Added Registration Activity

--- app/src/main/java/com/example/etechk/MainActivity.java
+++ app/src/main/java/com/example/etechk/MainActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogin, btnSignup;
-    @Override
+    Button btnLogin, btnRegister;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogin=findViewById(R.id.btnUserLogin);
-        btnSignup=findViewById(R.id.btnUserSignup);
+        btnRegister=findViewById(R.id.btnRegister);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/etechk/RegisterActivity.java
+++ app/src/main/java/com/example/etechk/RegisterActivity.java
@@ -2,16 +2,12 @@ package com.example.etechk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_register);
     }
 }

6d1a1c5db9be47deb3ab7e7ab4c377fe3238caf3
davidCabrera11 <davidcabrera052196@gmail.com>
2019-04-10 23:21:21 -0700

    Added Firebase

--- app/src/main/java/com/example/etechk/RegisterActivity.java
+++ app/src/main/java/com/example/etechk/RegisterActivity.java
@@ -1,13 +1,21 @@
 package com.example.etechk;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -53,9 +61,9 @@ public class RegisterActivity extends AppCompatActivity {
 
                 }else {
 
-                    String strFirstName = uFirstName.getText().toString().trim();
-                    String strLastName = uLastName.getText().toString().trim();
-                    String strEmail = uEmail.getText().toString().trim();
+                    final String strFirstName = uFirstName.getText().toString().trim();
+                    final String strLastName = uLastName.getText().toString().trim();
+                    final String strEmail = uEmail.getText().toString().trim();
                     String strPassword = uPassword.getText().toString().trim();
                     String strConfirmPassword = uConfirmPassword.getText().toString().trim();
 
@@ -89,6 +97,40 @@ public class RegisterActivity extends AppCompatActivity {
                         userConfirmPasswordWrapper.requestFocus();
                         return;
                     }
+
+                    mAuth.createUserWithEmailAndPassword(strEmail,strPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                //We need to add additional info to firebase
+                                User user = new User(strFirstName,strLastName,strEmail);
+                                FirebaseDatabase.getInstance().getReference("Users")
+                                        .child(FirebaseAuth.getInstance().getCurrentUser()
+                                                .getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            Toast.makeText(RegisterActivity.this,"User Created Successfully",Toast.LENGTH_LONG).show();
+                                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                            startActivity(intent);
+
+                                        }else{
+                                            Toast.makeText(RegisterActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                                        }
+
+                                    }
+                                });
+
+                            }else{
+                                Toast.makeText(RegisterActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                            }
+
+
+                        }
+                    });
 
                 }
 
